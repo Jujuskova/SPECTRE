b8a431779bc72da87f9874b6e3a85712c6d00c95
Florent <faducase@gmail.com>
2017-10-11 00:21:38 +0200

    fix

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+fonts_dir = "fonts"
+output_style = :nested
+relative_assets = true
+line_comments = false
+sass_options = {:debug_info=>false}
+sourcemap = false
